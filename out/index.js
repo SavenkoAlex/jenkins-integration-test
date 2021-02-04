ffb6082e6047c428f3dd7321ed4d3e54d198af8e
Savenko Aleksandr <sanyasavva.ss@gmail.com>
2021-02-04 13:15:54 +0300

    :q

---
+++ out/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function testRun(isInversible) {
+    if (isInversible === void 0) { isInversible = false; }
+    return !isInversible;
+}
+testRun();
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+function testRun(isInversible: boolean = false): Boolean {
+  return !isInversible
+}
+
+testRun()
